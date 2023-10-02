321e7f53c15b86e6e4174332572da75d8c1d20ac
Henry Herrera <110709796+henryhrra@users.noreply.github.com>
2023-10-01 23:57:58 -0600

    Add new CSS effects

--- script.js
+++
@@ -1,9 +0,0 @@
-/* let a = document.getElementById("navResume");
-a.addEventListener("click",()=>{
-    window.scroll(0,100,"smooth",);
-}); */
-document.getElementById("navResume").addEventListener("click",()=>{
-    //window.scroll(0,100,"smooth",);
-    let txt = document.getElementById("navResume").offsetTop;
-    console.log(txt);
-});
